3959d3bc1e13eaeadde61b7f01b463e278cfc29f
m_melamed <m_melamed@wargaming.net>
2025-12-11 17:12:15 +0200

    [2025] Refactorings and style fixes

--- src/main/kotlin/days/twentyfive/Day1.kt
+++ src/main/kotlin/days/twentyfive/Day1.kt
@@ -13,42 +13,41 @@ import kotlin.math.absoluteValue
 class Day1 : DayTask {
     override val spec1: TaskSpec
         get() = TaskSpec(
-                "L68\n" +
-                        "L30\n" +
-                        "R48\n" +
-                        "L5\n" +
-                        "R60\n" +
-                        "L55\n" +
-                        "L1\n" +
-                        "L99\n" +
-                        "R14\n" +
-                        "L82", "3"
+            "L68\n" +
+                    "L30\n" +
+                    "R48\n" +
+                    "L5\n" +
+                    "R60\n" +
+                    "L55\n" +
+                    "L1\n" +
+                    "L99\n" +
+                    "R14\n" +
+                    "L82", "3"
         )
     override val spec2: TaskSpec
         get() = TaskSpec(
-                "L68\n" +
-                        "L30\n" +
-                        "R48\n" +
-                        "L5\n" +
-                        "R60\n" +
-                        "L55\n" +
-                        "L1\n" +
-                        "L99\n" +
-                        "R14\n" +
-                        "L82", "6"
+            "L68\n" +
+                    "L30\n" +
+                    "R48\n" +
+                    "L5\n" +
+                    "R60\n" +
+                    "L55\n" +
+                    "L1\n" +
+                    "L99\n" +
+                    "R14\n" +
+                    "L82", "6"
         )
 
     private fun parseInput(input: List<String>): Iterable<Int> = input
-            .map { Pair(it[0], it.substring(1)) }
-            .map { (dir, step) -> (if (dir == 'R') 1 else -1) * step.toInt() }
+        .map { Pair(it[0], it.substring(1)) }
+        .map { (dir, step) -> (if (dir == 'R') 1 else -1) * step.toInt() }
 
     override fun task1(input: List<String>, params: ParamsMap): String =
-            parseInput(input)
-                    .steppedReduce(50)
-                    { current, step ->
-                        (current + step).mod(100)
-                    }.count { it == 0 }
-                    .toString()
+        parseInput(input)
+            .steppedReduce(50) { current, step ->
+                (current + step).mod(100)
+            }.count { it == 0 }
+            .toString()
 
     override fun task2(input: List<String>, params: ParamsMap): String {
         val input = parseInput(input)
